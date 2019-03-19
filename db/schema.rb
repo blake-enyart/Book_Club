79790e9ad20dcecf20f75f0da734ed6cfcc21480
Blake <blake.enyart@gmail.com>
2019-03-19 07:27:15 -0600

    Creates test for validations of title, rating, text in review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190319122854) do
+ActiveRecord::Schema.define(version: 20190319132204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20190319122854) do
     t.integer "number_of_pages"
     t.integer "year_published"
     t.string "book_cover_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.integer "rating"
+    t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
