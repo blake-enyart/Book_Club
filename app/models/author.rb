1a69d972d8dea61f73689ce7267a424ccb554d6d
Blake <blake.enyart@gmail.com>
2019-03-19 12:00:55 -0600

    Create many to many relationship between Author and Book and tested

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
 class Author < ApplicationRecord
-  has_many :books, through: :author_books
-  
+  has_many :authors_books
+  has_many :books, through: :authors_books
+
   validates_presence_of :name
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 describe Author, type: :model do
+
   describe "validations" do
     it { should validate_presence_of :name }
   end
 
   describe "relationships" do
-    it { should have_many(:books).through(:author_books) }
+    it { should have_many :authors_books }
+    it { should have_many(:books).through(:authors_books) }
   end
 end
