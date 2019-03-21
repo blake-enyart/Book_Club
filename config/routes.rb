2f99cd0c6e724c00d7ef78de1c4fcb3f2a502b2c
Blake <blake.enyart@gmail.com>
2019-03-21 07:22:21 -0600

    Passed feature test display all authors

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books, only:[:index]
+  resources :books, only:[:index, :show]
 
 end
