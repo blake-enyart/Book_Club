deb788e2dd61aa123b5c8d55bab599b5a504481a
Blake <blake.enyart@gmail.com>
2019-03-21 07:31:08 -0600

    Minor edit to messaging in test

--- spec/features/books/user_sees_all_books_spec.rb
+++ spec/features/books/user_sees_all_books_spec.rb
@@ -2,12 +2,13 @@ require "rails_helper"
 
 RSpec.describe "user sees all books", type: :feature do
   describe 'user visits /books' do
-    it "displays all books" do
+    it "displays all books correctly" do
       book_1 = Book.create(title: "Where the Crawdads Sing", number_of_pages: 384, year_published: 2018, book_cover_url: "https://prodimage.images-bn.com/pimages/9780735219090_p0_v10_s550x406.jpg")
       book_2 = Book.create(title: "East of Eden", number_of_pages: 608, year_published: 1952, book_cover_url: "https://upload.wikimedia.org/wikipedia/en/5/56/EastOfEden.jpg")
 
       author_1 = book_1.authors.create(name: "Delia Owens")
       author_2 = book_2.authors.create(name: "John Steinbeck")
+      author_2 = book_2.authors.create(name: "Other Guy")
 
       visit books_path
 
@@ -21,7 +22,7 @@ RSpec.describe "user sees all books", type: :feature do
       within "\##{book_2.id}" do
         expect(page).to have_content(book_2.title)
         expect(page).to have_content("Number of Pages: #{book_2.number_of_pages}")
-        expect(page).to have_content("Author(s): John Steinbeck")
+        expect(page).to have_content("Author(s): John Steinbeck, Other Guy")
         expect(page).to have_content("Year Published: #{book_2.year_published}")
       end
     end
