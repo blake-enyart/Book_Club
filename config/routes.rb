47ba6e956a94bfcbb3c3626224619b9335331434
Zach Leach <zachleach@techoverco.com>
2019-03-25 12:19:39 -0600

    Made route to destroy method.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books, only:[:index, :show, :new, :create, :destroy]
-  resources :authors, only:[:index, :show]
+  resources :authors, only:[:index, :show, :destroy]
 end
