1bb25365d44f776cf86af7a1e8a2d94bc26c9cb0
Blake <blake.enyart@gmail.com>
2019-03-25 09:32:02 -0600

    Add model book highest number of pages test and passed

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -87,10 +87,16 @@ describe Book, type: :model do
       expected = [@book_3, @book_1, @book_2, @book_4]
 
       expect(actual).to eq(expected)
-      
+
       #lowest averate review test
       actual = Book.sort_by(['AVG(reviews.rating)', 'ASC'])
       expected = [@book_3, @book_1, @book_2, @book_4].reverse
+
+      expect(actual).to eq(expected)
+
+      #highest number of pages test
+      actual = Book.sort_by(['books.number_of_pages', 'DESC'])
+      expected = [@book_2, @book_4, @book_1, @book_3]
 
       expect(actual).to eq(expected)
     end
